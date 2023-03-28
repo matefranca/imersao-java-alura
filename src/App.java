60cc7c575c5f2e45e4590f7b71b83694e6c53322
Matheus Waked Franca <matheus363@hotmail.com>
2023-03-28 01:27:28 -0300

    Day 1

---
+++ src/App.java
@@ -0,0 +1,30 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI uri = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(uri).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> movies = parser.parse(body);
+
+        for (Map<String,String> movie : movies) {
+            System.out.println(movie.get("title"));
+            System.out.println(movie.get("image"));
+            System.out.println(movie.get("imDbRating"));
+        }
+    }
+}
